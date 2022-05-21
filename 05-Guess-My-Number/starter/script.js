0d3b9f4086458b18dae77717474b54d0656b8d59
Darielquinta <65695779+Darielquinta@users.noreply.github.com>
2022-05-21 10:56:06 -0400

    aaaa

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,4 +6,5 @@ document.querySelector('.message').textContent = "Correct Number! 🏅"
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').textContent;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
